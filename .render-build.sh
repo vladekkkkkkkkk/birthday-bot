46f0e2226cf94a5e67f2b36bbb2ee68bd8a93e0d
vladekkkkkkkkk <119927814+vladekkkkkkkkk@users.noreply.github.com>
2025-06-25 16:43:53 +0300

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
 echo "🧹 Cleaning up cache manually"
-pip uninstall -y python-telegram-bot
-pip install python-telegram-bot==20.7
+pip uninstall -y python-telegram-bot || true
+
+echo "🔁 Forcing clean reinstall of PTB"
+pip install --upgrade --force-reinstall python-telegram-bot==20.7
+
+echo "📦 Installing remaining requirements"
+pip install -r requirements.txt
