73efed8020eebf0fc628b9273205ec152cee04c2
vladekkkkkkkkk <119927814+vladekkkkkkkkk@users.noreply.github.com>
2025-06-25 17:09:08 +0300

    delete render-bild script

--- .render-build.sh
+++ .render-build.sh
@@ -1,7 +1,1 @@
-#!/usr/bin/env bash
 
-echo "🧹 Cleaning up cache manually"
-pip uninstall -y python-telegram-bot || true
-
-echo "🔁 Forcing clean reinstall of PTB 20.7"
-pip install --upgrade --force-reinstall python-telegram-bot==20.7
